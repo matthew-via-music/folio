35365fdfa744abd67aca207073cb754af6a28c7e
matthew-via-music <mvm@matthewviamusic.com>
2023-03-03 23:03:37 +0000

    try workaround for Modal/rollup build failure

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://folio.matthew-via-music.com',
-  baseUrl: ".",
   integrations: [tailwind(), react()]
 });
