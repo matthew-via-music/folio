90df07e04748c303b63b08129e8cb5a25d02dea7
matthew-via-music <mvm@matthewviamusic.com>
2023-03-03 21:38:12 +0000

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://folio.matthew-via-music.com',
   integrations: [tailwind(), react()]
 });
